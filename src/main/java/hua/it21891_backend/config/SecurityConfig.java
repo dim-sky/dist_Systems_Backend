94496271ee0754fbc3420eeef3947822c60d3c4a
Petros Vougioukas <petrosvougiou00@gmail.com>
2025-06-30 17:07:01 +0300

    removing cors conf

--- src/main/java/hua/it21891_backend/config/SecurityConfig.java
+++ src/main/java/hua/it21891_backend/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         
         // ALLOW EVERYTHING - for development only
-        config.setAllowedOriginPatterns(List.of("http:/localhost:9090")); // Allows all origins
+        config.setAllowedOriginPatterns(List.of("*")); // Allows all origins
         config.setAllowedMethods(List.of("*")); // Allows all HTTP methods
         config.setAllowedHeaders(List.of("*")); // Allows all headers
         config.setAllowCredentials(false); // MUST be false when using "*"
